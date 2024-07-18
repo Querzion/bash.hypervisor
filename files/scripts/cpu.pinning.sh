ae01341e56a5915099e07a36c5fb1e13a7d5fd31
Slisk Lindqvist <slisk.lindqvist@querzion.com>
2024-07-18 17:53:31 +0200

    Create cpu.pinning.sh

---
+++ files/scripts/cpu.pinning.sh
@@ -0,0 +1,19 @@
+### _____________________________________________________________________________________________________________
+###
+###                             CPU PINNING (https://youtu.be/WYrTajuYhCk?t=725)
+### _____________________________________________________________________________________________________________
+
+#lscpu -e    # See the CPU cores
+
+
+# PASSTHROUGH GPU - (https://www.youtube.com/watch?v=BUSrdUoedTo | https://www.youtube.com/watch?v=WYrTajuYhCk | https://www.youtube.com/watch?v=3yhwJxWSqXI)
+#sudo pacman -S tree
+
+
+#https://wiki.archlinux.org/title/PCI_passthrough_via_OVMF
+#https://passthroughpo.st/simple-per-vm-libvirt-hooks-with-the-vfio-tools-hook-helper/
+#https://github.com/joeknock90/Single-GPU-Passthrough
+#https://www.reddit.com/r/VFIO/
+
+# PATCHED BIOS
+#https://www.techpowerup.com/vgabios/192500/asus-rx580-8192-170328 (ASUS AMD RX580 GPU BIOS)
